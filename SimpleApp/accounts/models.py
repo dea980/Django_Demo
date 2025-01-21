e45d0f368ce93f7a29378236b9122d9efd27d266
dan.kim <dan.kim@indyaudiolabs.com>
2025-01-21 15:21:02 +0900

    Add authentication system with django-allauth and UserProfile model

--- SimpleApp/accounts/models.py
+++ SimpleApp/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    avatar = models.URLField(max_length=1024, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create a UserProfile instance when a new User is created"""
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Save the UserProfile instance when the User is saved"""
+    instance.profile.save()
